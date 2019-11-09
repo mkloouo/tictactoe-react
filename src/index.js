3427af4b00f658dc052b5b2d8658086c4746138e
Mykola Odnosumov <mkloouo@gmail.com>
2019-11-09 21:26:15 +0200

    Rewrite board render in two loops
    Signed-off-by: Mykola Odnosumov <mkloouo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -23,25 +23,17 @@ class Board extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+        const boardRows = [];
+
+        for (let row = 0; row < 3; ++row) {
+            const squares = [];
+            for (let col = 0; col < 3; ++col) {
+                squares.push(this.renderSquare(row * 3 + col));
+            }
+            boardRows.push(<div className="board-row">{squares}</div>);
+        }
+
+        return (<div>{boardRows}</div>);
     }
 }
 
